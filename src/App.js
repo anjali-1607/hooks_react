b206e2efb50affeacb5b0226126879fc9233df6a
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-06-08 18:01:44 +0530

    UseReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import UseEffect from "./UseEffect";
 import UseState from "./UseState";
+import UseReducer from "./UseReducer";
 
 const App = () => {
-  return <UseState />;
-  return <UseEffect />;
+  // return <UseState />;
+  // return <UseEffect />;
+  return <UseReducer />;
 };
 
 export default App;
